263e3540e167cfc0bfd566a79c949620e8a24d1f
Samora Machel <samora@me.laptop>
2023-02-05 09:02:24 +0300

    Added bottom navigation menu and home page activity design

--- app/src/main/java/com/example/paypesa/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/paypesa/ui/auth/AuthActivity.kt
@@ -1,16 +1,35 @@
 package com.example.paypesa.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.paypesa.databinding.ActivityAuthBinding
+import com.example.paypesa.ui.home.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
+    private lateinit var authViewModel: AuthViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+
+        binding.authLogin.setOnClickListener {
+            navigateToHomeScreen()
+        }
+    }
+
+    private fun navigateToHomeScreen() {
+        val intent = Intent(applicationContext, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
